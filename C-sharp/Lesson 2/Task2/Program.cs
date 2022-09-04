04aae2d4e29012bfe7ece2a7315724fdd481b14b
Dastan <super.dastno@yandex.ru>
2022-09-05 02:35:40 +0300

    Add 2nd Task

---
+++ C-sharp/Lesson 2/Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿void randomArray (int[] array){
+    for (int i = 0; i < array.Length; i++) {
+        array[i] = new Random().Next(1, 10);
+    }
+}
+int[] array = new int[3];
+randomArray(array);
+System.Console.WriteLine($"{array[0]}{array[1]}{array[2]}");
+System.Console.WriteLine($"{array[0]}{array[2]}");

e7dd1bc5d69120b570b87d557bee290675de5e89
Dastan <super.dastno@yandex.ru>
2022-09-05 01:24:56 +0300

    Add new folder with L1, L2

---
+++ Lesson 2/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(value: message); //Вывести сообщение
+    string readValue = Console.ReadLine(); //Ситывает с консоли строку
+    int result = int.Parse(s: readValue); //Преобразует строку в целое число
+    return result; //Возвращает результат
+}
+
+int userNum = Prompt(message: "Введите 3-х значное число > ");
+
+int result = userNum / 10 % 10;
+Console.WriteLine(value: result);
